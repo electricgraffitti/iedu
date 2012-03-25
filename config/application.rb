7644112480a1b3046283c63e8bbba271b54a6752
Bob Hanson <bob@cube2media.com>
2012-03-24 19:02:27 -0600

    eager load config deploy error fix

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Iedu
   class Application < Rails::Application
+
+    # don't attempt to auto-require the moonshine manifests into the rails env
+    config.paths['app/manifests'] = 'app/manifests'
+    config.paths['app/manifests'].skip_eager_load!
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
